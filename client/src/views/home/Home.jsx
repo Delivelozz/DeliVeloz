933941d444581ccd1b0af74a21c8a0f27a689c50
SoniaMEGS <megs.sonia@gmail.com>
2024-03-18 22:06:03 -0700

    Feature: Cards.
    ## Description:
    
    - Llamar a la FAKE API en el home para crear platillos en el store.
    - Utilizar el componente Cards para presentar los platillos del store.
    - Crear un shoppingCart para agregar platillos al store.
    - Crear el UI para el componente Cards.

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,7 +1,23 @@
-import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setDishes } from "../../redux/actions/actions.js";
+import { getDishes } from "../../data/index.js";
 
 export default function Home() {
+  const dishes = useSelector((state) => state.dishes);
+  const dispatch = useDispatch();
+
+  //Function that calls data
+  useEffect(() => {
+    const fetchDishes = async () => {
+      const dishesRes = await getDishes();
+      dispatch(setDishes(dishesRes));
+    };
+
+    fetchDishes();
+  }, [dispatch]);
+
+  //console.log(dishes);
   return (
     <section className="container">
       <h1>Home</h1>
