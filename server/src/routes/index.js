ebdeb35598a0f335b2f019f87b0b1b2ed10234b3
Laxwip <pachecogam135583@gmail.com>
2024-03-20 08:43:48 -0500

    editProduct-init

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,10 +5,31 @@ const router = Router();
 // Importar controladores
 const getAllProducts = require('../controllers/getAllProducts');
 const getProductById = require('../controllers/getProductById');
+const editProduct = require('../controllers/editProduct');
 
 // Definir rutas y controladores
 router.get("/productos", getAllProducts)
 router.get("/productos/:id", getProductById)
+router.patch("/productos/:id", editProduct);
+
+/*
+  como se deberia de manejar en el front:
+  
+fetch('/productos/1', {
+ method: 'PATCH',
+ headers: {
+    'Content-Type': 'application/json'
+ },
+ body: JSON.stringify({
+    nombre: 'Nuevo nombre del producto',
+    precio: 19.99
+    // ...otros campos que se quieren actualizar
+ })
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+*/
 
 
 module.exports = router;
