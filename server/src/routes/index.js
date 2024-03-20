af02d4e326476ace14b292adeb6ea79c3126dcd9
luischapid <luischapid03@gmail.com>
2024-03-20 09:56:10 -0500

    controllers de valoracion post, delete y update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
 const getAllProducts = require('../controllers/getAllProducts');
 const startDatabase = require('../controllers/startDatabase');
+
+
+//valoracion
+const postValoracion = require('../controllers/postValoracion')
+const updateValoracion= require('../controllers/UpdateValoracion')
+const deleteValoracion= require('../controllers/deleteValoracion')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -12,6 +18,9 @@ const router = Router();
 router.get("/products", getAllProducts)
 // router.get("/", startDatabase)
 
+router.post("/valoracion", postValoracion)
+router.put("/valoracion/:id", updateValoracion)
+router.delete("/valoracion/:id", deleteValoracion)
 
 
 module.exports = router;
