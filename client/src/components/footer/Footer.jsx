bb66d70d4b15adf9d782748fd60aa05fd00b5feb
fedecodelab <federicoguzman.css@gmail.com>
2024-03-20 11:58:25 -0300

    Slider terminado y aplicado al home

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="bg-sundown-500 text-white p-12 flex justify-center items-center flex-col gap-3 mt-16">
       <h4 className="text-2xl">DeliVeloz</h4>
-      <p>Since 2024 - Recibe tu comida al instante ❤️‍🔥</p>
+      <p>Since 2024 - Recibe tu comida al instante 🍕🔥</p>
     </div>
   );
 }
--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,11 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import Slider from "react-slick";
+import SliderHome from "../../components/sliderHome/Slider";
 
 export default function Home() {
   return (
-    <section className="container">
-      <h1>Home</h1>
+    <section>
+      <SliderHome />
+      <div className="container">
+        <h1 className="text-center text-xl">
+          Nuestras <span className="text-sundown-500">Novedades</span>
+        </h1>
+      </div>
     </section>
   );
 }
