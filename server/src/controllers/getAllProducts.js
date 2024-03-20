fc21218a2200372ae35bedab1f1d8d2e48b7318d
Laxwip <pachecogam135583@gmail.com>
2024-03-19 21:17:10 -0500

    act_index&getAllProducts&getProductById

--- server/src/controllers/getAllProducts.js
+++ server/src/controllers/getAllProducts.js
@@ -1,14 +1,16 @@
-// import { Product } from "../db.js";
 const { Producto } = require("../db.js");
-// const axios = require("axios");
 
 const getAllProducts = async (req, res) =>{
   try {
     const data = await Producto.findAll();
+    //Verificamos si hay productos en la base de datos
+    if (data?.length === 0) {
+      return res.status(404).json({ error: "No se encontraron productos" });
+    }
     res.status(200).json(data)
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Error al recuperar los productos" });
   }
 }
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,9 +4,11 @@ const router = Router();
 
 // Importar controladores
 const getAllProducts = require('../controllers/getAllProducts');
+const getProductById = require('../controllers/getProductById');
 
 // Definir rutas y controladores
-router.get("/products", getAllProducts)
+router.get("/productos", getAllProducts)
+router.get("/productos/:id", getProductById)
 
 
 module.exports = router;
