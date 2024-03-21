857a9d727469857ce30dee51c0d661504922eefc
fedecodelab <federicoguzman.css@gmail.com>
2024-03-21 18:33:49 -0300

    paginación terminada

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,7 +1,61 @@
-export default function Pagination() {
+import PaginationPrev from "../icons/PaginationPrev";
+import PaginationNext from "../icons/PaginationNext";
+
+export default function Pagination({
+  totalPosts,
+  postsPerPage,
+  setCurrentPage,
+  currentPage,
+}) {
+  // ? -------------------------------- Logica
+
+  let totalPages = Math.ceil(totalPosts / postsPerPage);
+  let pages = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+  // ? -------------------------------- Handles de anterior y siguiente
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
-    <div>
-      <h1>Pagination</h1>
+    <div className="flex justify-center items-center gap-3 mt-6">
+      <button
+        onClick={handlePrevPage}
+        disabled={currentPage === 1}
+        className={currentPage === 1 ? "disabled" : "active"}
+      >
+        <PaginationPrev />
+      </button>
+      {pages.map((page, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => setCurrentPage(page)}
+            className={currentPage === page ? "current" : "active"}
+          >
+            {page}
+          </button>
+        );
+      })}
+      <button
+        className={currentPage === totalPages ? "disabled" : "active"}
+        disabled={currentPage === totalPages}
+        onClick={handleNextPage}
+      >
+        <PaginationNext />
+      </button>
     </div>
   );
 }
--- client/src/views/products/Products.jsx
+++ client/src/views/products/Products.jsx
@@ -1,29 +1,37 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setDishes } from "../../redux/actions/actions.js";
-import { getDishes } from "../../data/index.js";
 import Cards from "../../components/cards/Cards.jsx";
 import Filters from "../../components/filters/Filters.jsx";
+import Pagination from "../../components/pagination/Pagination.jsx";
 
 export default function Products() {
   const dishes = useSelector((state) => state.dishes);
   const dispatch = useDispatch();
 
-  //Function that calls data
   useEffect(() => {
-    const fetchDishes = async () => {
-      const dishesRes = await getDishes();
-      dispatch(setDishes(dishesRes));
-    };
-
-    fetchDishes();
+    dispatch(setDishes());
   }, [dispatch]);
+
+  // ? -------------------------------- Paginate
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(16);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = dishes.slice(firstPostIndex, lastPostIndex);
 
   return (
     <section className="container">
       <Filters />
-
-      <Cards dishes={dishes} />
+      <Cards dishes={currentPosts} />
+      <Pagination
+        totalPosts={dishes.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </section>
   );
 }
