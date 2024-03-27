83d2cc7573f2e08e447dcfd64344e25f2436b8a4
Maryxale <romerobencomo@gmail.com>
2024-03-27 17:23:39 -0300

    cambios

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,16 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require("cors");
 
 require("./db.js");
 
 const server = express();
+
+
+
+
+
 
 server.name = "API";
 
@@ -16,7 +22,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');  update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.header("Access-Control-Allow-Origin", "*");//http://127.0.0.1:5173
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -26,7 +32,14 @@ server.use((req, res, next) => {
   next();
 });
 
+const corsOptions = {origin: '*',}
+
 server.use("/", routes);
+
+server.use(cors(corsOptions));
+
+
+
 
 // Error catching endware.
 
