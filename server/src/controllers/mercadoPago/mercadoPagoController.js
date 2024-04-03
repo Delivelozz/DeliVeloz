97cd93e2d84e5436a6eacbb22dd6cbf8afdaf41f
gastondandrea <gastondandrea@hotmail.com>
2024-04-03 14:07:06 -0300

    Mercado pago funcional

--- server/src/controllers/mercadoPago/mercadoPagoController.js
+++ server/src/controllers/mercadoPago/mercadoPagoController.js
@@ -1,5 +1,55 @@
-const mercadoPagoController = () => {
-    return 1;
+// SDK de Mercado Pago
+const { MercadoPagoConfig, Preference } = require('mercadopago');
+// Agrega credenciales
+const client = new MercadoPagoConfig({ accessToken: process.env.YOUR_ACCESS_TOKEN });
+
+const mercadoPagoController = async (itemsNew) => {
+    const body = {
+        items: itemsNew,
+        back_urls: {
+          success: "https://www.youtube.com/watch?v=-VD-l5BQsuE&t=47s",
+          failure: "https://www.youtube.com/watch?v=-VD-l5BQsuE&t=47s",
+          pending: "https://www.youtube.com/watch?v=-VD-l5BQsuE&t=47s"
+        },
+        payment_methods: {
+            excluded_payment_methods: [
+                {
+                    id: "argencard"
+                },
+                {
+                    id: "cabal"
+                },
+                {
+                    id: "cmr"
+                },
+                {
+                    id: "cencosud"
+                },
+                {
+                    id: "cordobesa"
+                },
+                {
+                    id: "diners"
+                },
+                {
+                    id: "naranja"
+                },
+                {
+                    id: "tarshop"
+                }
+            ],
+            excluded_payment_types: [
+                {
+                    id: "ticket"
+                }
+            ],
+            installments: 1
+        },
+        auto_return: "approved"
+      }
+      const preference = new Preference(client);
+      const result = await preference.create({body});
+      return result;
 };
 
 module.exports = mercadoPagoController;
--- server/src/handlers/mercadoPago/mercadoPagoHandler.js
+++ server/src/handlers/mercadoPago/mercadoPagoHandler.js
@@ -1,13 +1,8 @@
-// SDK de Mercado Pago
-const { log } = require('console');
-const { MercadoPagoConfig, Preference } = require('mercadopago');
-// Agrega credenciales
-const client = new MercadoPagoConfig({ accessToken: process.env.YOUR_ACCESS_TOKEN });
+const mercadoPagoController = require('../../controllers/mercadoPago/mercadoPagoController');
 
 const mercadoPagoHandler = async (req, res) => {
   const items = req.body;
   try {
-    
     const itemsNew = items.map((item)=>{
       return {
         title: item.name,
@@ -16,42 +11,11 @@ const mercadoPagoHandler = async (req, res) => {
         current_id: "ARS",
       }
     })
-    console.log("item", itemsNew);
-
-    // const items = req.body.items.map(item => ({
-    //   title: item.title,
-    //   quantity: Number(item.quantity),
-    //   unit_price: Number(item.price),
-    //   current_id: "ARS",
-    // }));
-
-    const body = {
-      // itemsNew: [
-      //   {
-      //     title: "Hamburguesa",
-      //     quantity: 2,
-      //     unit_price: 200,
-      //     current_id: "ARS",
-      //   }
-      // ],
-      itemsNew,
-      back_urls: {
-        success: "https://www.youtube.com/watch?v=-VD-l5BQsuE&t=47s",
-        failure: "https://www.youtube.com/watch?v=-VD-l5BQsuE&t=47s",
-        pending: "https://www.youtube.com/watch?v=-VD-l5BQsuE&t=47s"
-      },
-      auto_return: "approved"
-    }
-    const preference = new Preference(client);
-    const result = await preference.create({body});
-    res.json({
-      id: result.id,
-    })
+    const response = await mercadoPagoController(itemsNew); 
+    res.json({id: response})
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: "Error al crear preferencia :("
-    })
+    console.log(error)
+    res.status(500).json({ error: "Error al crear preferencia" })
   }
 }
 
