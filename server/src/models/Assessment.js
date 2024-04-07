974d78ad9a6cf90a40ed346bb4cf3719e941d014
luischapid <luischapid03@gmail.com>
2024-04-07 14:39:48 -0500

    rutas de post y get assessment funcional

--- server/src/models/Assessment.js
+++ server/src/models/Assessment.js
@@ -21,7 +21,7 @@ module.exports = (sequelize) => {
       },
       productId: {
         type: DataTypes.INTEGER, 
-        allowNull: false,
+        allowNull: true,
     }},
   
     { timestamps: false });
