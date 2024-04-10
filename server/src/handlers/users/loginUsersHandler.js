b1431fe0bd8ab6acffbdf470f0d438c58e3ec22b
Laxwip <pachecogam135583@gmail.com>
2024-04-09 19:05:08 -0500

    login

--- server/src/handlers/users/loginUsersHandler.js
+++ server/src/handlers/users/loginUsersHandler.js
@@ -1,10 +1,19 @@
+const jwt = require('jsonwebtoken'); 
 const loginUserController = require("../../controllers/users/loginUserController");
 
 const loginUsersHandler = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const response = await loginUserController(email, password);
-    res.status(200).json(response);
+    const user = await loginUserController(email, password);
+    const token = jwt.sign(
+      {
+        email: user.email,
+      },
+      "clave_secreta_de_prueba"
+      // process.env.JWT_SECRET,//Crear en el .env
+      // { expiresIn: "1h" } //Por si se necesita un tiempo de expiración
+    );
+    res.status(200).json({ message: "Inicio de sesión exitoso", user, token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
