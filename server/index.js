b6b22f80695f23494756917987749aec14c40d27
Laxwip <pachecogam135583@gmail.com>
2024-04-10 14:31:55 -0500

    alterFalse

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const startDatabase = require('./src/functions/startDatabase.js');
 const { conn } = require('./src/db.js');
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ alter: false }).then(() => {
   startDatabase().then(() => {
     server.listen(3001, () => {
       console.log('Server listening at 3001'); // eslint-disable-line no-console
