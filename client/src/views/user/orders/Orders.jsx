170762f937b457998667ee6de53773caed55836d
fedecodelab <federicoguzman.css@gmail.com>
2024-04-15 19:02:13 -0300

    cambios

--- client/src/views/user/orders/Orders.jsx
+++ client/src/views/user/orders/Orders.jsx
@@ -21,9 +21,52 @@ export default function Orders() {
   const myOrders = useSelector((state) => state.myOrders);
   console.log(myOrders);
 
+  const columns = [
+    {
+      name: "Número de pedido",
+      selector: (row) => row.id,
+      sortable: true,
+      width: "160px",
+    },
+    {
+      name: "Estado de la orden",
+      selector: (row) => row.orderStatus,
+      width: "150px",
+    },
+    {
+      name: "Precio total",
+      selector: (row) => row.total,
+      sortable: true,
+      width: "150px",
+    },
+    {
+      name: "Productos",
+      cell: (row) => (
+        <div>
+          {row.products.map((product) => (
+            <div key={product.id}>
+              <p>Nombre: {product.name}</p>
+              <p>Descripción: {product.description}</p>
+              <p>Precio: {product.price}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
+      ),
+      sortable: true,
+      width: "300px",
+    },
+  ];
+
   return (
     <section className="container">
+<<<<<<< HEAD
       <h1 className="mt-4">Tablas de pedidos</h1>
+=======
+      <h1>Tablas de pedidos</h1>
+
+      <DataTable columns={columns} data={myOrders} selectableRows pagination />
+>>>>>>> 8d9b221 (cambios)
     </section>
   );
 }
