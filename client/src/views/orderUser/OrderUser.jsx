82335f2b8ba130a10c7e9e3571d9ca7f8a26ada0
SoniaMEGS <megs.sonia@gmail.com>
2024-04-16 14:48:54 -0700

    local storage para que no se desloge

--- client/src/views/orderUser/OrderUser.jsx
+++ client/src/views/orderUser/OrderUser.jsx
@@ -1,6 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { getOrder } from "../../redux/actions/actions";
+import { useLocalStoreUserData } from "../../hooks/useLocalStoreUserData.js";
+import { useLocalStoreUserDataGoogle } from "../../hooks/useLocalStoreUserDataGoogle.js";
+import { useGetShoppingDB } from "../../hooks/useGetShoppingDB.js";
 
 export default function OrderUser() {
   const orderDetail = useSelector((state) => state.orderDetail);
@@ -8,6 +11,10 @@ export default function OrderUser() {
   const idOrder = useSelector((state) => state.idOrder);
   const [idUser, setIdUser] = useState(null);
   const dispatch = useDispatch();
+
+  useLocalStoreUserData();
+  useLocalStoreUserDataGoogle();
+  useGetShoppingDB();
 
   //console.log(idOrder);
 
@@ -23,24 +30,24 @@ export default function OrderUser() {
     }
   }, [idUser, idOrder]);
 
-  console.log(orderDetail);
+  //console.log(orderDetail);
 
   return (
     <section className="container">
       <h2>Detalles del pedido</h2>
-      <p>User ID: {orderDetail.userId}</p>
-      <p>Total a pagar: ${orderDetail.total.toFixed(2)}</p>
+      <p>User ID: {orderDetail?.userId}</p>
+      <p>Total a pagar: ${orderDetail?.total}</p>
       <h3>Productos:</h3>
       <ul>
-        {orderDetail.products.map((product, index) => (
+        {orderDetail?.products?.map((product, index) => (
           <li key={index}>
-            {product.name} - Cantidad: {product.quantity} - Precio: $
-            {product.price}
+            {product?.name} - Cantidad: {product?.quantity} - Precio: $
+            {product?.price}
           </li>
         ))}
       </ul>
-      <p>Estado del pago: {orderDetail.paid ? "Pagado" : "No pagado"}</p>
-      <p>Estado del pedido: {orderDetail.orderStatus}</p>
+      <p>Estado del pago: {orderDetail?.paid ? "Pagado" : "No pagado"}</p>
+      <p>Estado del pedido: {orderDetail?.orderStatus}</p>
     </section>
   );
 }
