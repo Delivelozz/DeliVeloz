9ccc775a65b9a12e470bf65afb88917bf4cb7a7c
fedecodelab <federicoguzman.css@gmail.com>
2024-04-17 02:51:41 -0300

    añadiendo metricas

--- client/src/views/admin/dashboard/Dashboard.jsx
+++ client/src/views/admin/dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
 import Sidenav from "../../../components/admin/sidenav/Sidenav";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 
 export default function Dashboard() {
   return (
@@ -6,6 +8,42 @@ export default function Dashboard() {
       <Sidenav />
       <section className="container">
         <h1>Dashboard</h1>
+        <div>
+          <div>
+            <Bar
+              data={{
+                labels: ["A", "B", "C"],
+                datasets: [
+                  {
+                    label: "Revenue",
+                    data: [200, 300, 400],
+                  },
+                  {
+                    label: "Loss",
+                    data: [90, 80, 70],
+                  },
+                ],
+              }}
+            />
+          </div>
+          <div>
+            <Doughnut
+              data={{
+                labels: ["A", "B", "C"],
+                datasets: [
+                  {
+                    label: "Revenue",
+                    data: [200, 300, 400],
+                  },
+                  {
+                    label: "Loss",
+                    data: [90, 80, 70],
+                  },
+                ],
+              }}
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
