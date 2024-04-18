2fca4401d67074e96231dbe7ff8e25dca0895c69
Maryxale <romerobencomo@gmail.com>
2024-04-17 22:08:16 -0300

    avance

--- client/src/views/user/editUser/EditUser.jsx
+++ client/src/views/user/editUser/EditUser.jsx
@@ -100,84 +100,87 @@ export default function EditUser() {
           },
         }
       );
+      // alert(
+      //   "Ocurrió un error al editar el usuario. Por favor, inténtalo de nuevo."
+      // );
     }
-
-    return (
-      <section className="container">
-        <h1 className="mb-6 mt-6">
-          Editar <span className="text-sundown-500">Usuario</span>
-        </h1>
-        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
-          <div className="flex gap-5">
-            <div className="flex flex-col flex-1">
-              <label className="font-semibold text-sm text-gray-800 mb-1">
-                Nombre:
-              </label>
-              <input
-                value={userData.name}
-                onChange={handleChange}
-                type="text"
-                name="name"
-                placeholder="Añadir nombre"
-                className=" bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
-              />
-              {errors.name && <p className="error">{errors.name}</p>}
-            </div>
-
-            <div className="flex flex-col flex-1">
-              <label className="font-semibold text-sm text-gray-800 mb-1">
-                Apellido:
-              </label>
-              <input
-                value={userData.lastName}
-                onChange={handleChange}
-                className=" bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
-                type="text"
-                name="lastName"
-                placeholder="Añadir apellido "
-              />
-              {errors.lastName && <p className="error">{errors.lastName}</p>}
-            </div>
-          </div>
-
-          <div className="flex justify-between gap-5">
-            <div className="flex flex-col flex-1">
-              <label className="font-semibold text-sm text-gray-800 mb-1">
-                Teléfono:
-              </label>
-
-              <input
-                value={userData.phone}
-                onChange={handleChange}
-                className="bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
-                type="number"
-                name="phone"
-                placeholder="Añadir número de teléfono"
-              />
-              {errors.phone && <p className="error">{errors.phone}</p>}
-            </div>
-
-            <div className="flex flex-col flex-1">
-              <label className="font-semibold text-sm text-gray-800 mb-1">
-                Direccion:
-              </label>
-
-              <input
-                value={userData.userAddress}
-                onChange={handleChange}
-                className="bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
-                type="text"
-                name="userAddress"
-                placeholder="Añadir direccion"
-              />
-              {errors.userAddress && (
-                <p className="error">{errors.userAddress}</p>
-              )}
-            </div>
-          </div>
-          <button className="btn-bg">Confirmar cambios</button>
-        </form>
-      </section>
-    );
   };
+
+  return (
+    <section className="container">
+      <h1 className="mb-6 mt-6">
+        Editar <span className="text-sundown-500">Usuario</span>
+      </h1>
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <div className="flex gap-5">
+          <div className="flex flex-col flex-1">
+            <label className="font-semibold text-sm text-gray-800 mb-1">
+              Nombre:
+            </label>
+            <input
+              value={userData.name}
+              onChange={handleChange}
+              type="text"
+              name="name"
+              placeholder="Añadir nombre"
+              className=" bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
+            />
+            {errors.name && <p className="error">{errors.name}</p>}
+          </div>
+
+          <div className="flex flex-col flex-1">
+            <label className="font-semibold text-sm text-gray-800 mb-1">
+              Apellido:
+            </label>
+            <input
+              value={userData.lastName}
+              onChange={handleChange}
+              className=" bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
+              type="text"
+              name="lastName"
+              placeholder="Añadir apellido "
+            />
+            {errors.lastName && <p className="error">{errors.lastName}</p>}
+          </div>
+        </div>
+
+        <div className="flex justify-between gap-5">
+          <div className="flex flex-col flex-1">
+            <label className="font-semibold text-sm text-gray-800 mb-1">
+              Teléfono:
+            </label>
+
+            <input
+              value={userData.phone}
+              onChange={handleChange}
+              className="bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
+              type="number"
+              name="phone"
+              placeholder="Añadir número de teléfono"
+            />
+            {errors.phone && <p className="error">{errors.phone}</p>}
+          </div>
+
+          <div className="flex flex-col flex-1">
+            <label className="font-semibold text-sm text-gray-800 mb-1">
+              Direccion:
+            </label>
+
+            <input
+              value={userData.userAddress}
+              onChange={handleChange}
+              className="bg-gray-50 border border-gray-600 p-2 rounded-lg text-sm focus:outline-sundown-500 focus:border-transparent"
+              type="text"
+              name="userAddress"
+              placeholder="Añadir direccion"
+            />
+            {errors.userAddress && (
+              <p className="error">{errors.userAddress}</p>
+            )}
+          </div>
+        </div>
+        <button className="btn-bg">Confirmar cambios</button>
+      </form>
+    </section>
+  );
 }
